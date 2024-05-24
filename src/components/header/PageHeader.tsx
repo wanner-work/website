52f46ecd9d625cfd198fa734c88c8a8abe826eed
jwanner83 <jonas@wanner.com>
2024-05-25 08:52:29 +0930

    feat: adding dots for header

--- src/components/header/PageHeader.tsx
+++ src/components/header/PageHeader.tsx
@@ -15,9 +15,10 @@ interface Props {
 export default function PageHeader({ title, description, image, date }: Props) {
   return (
     <Box
-      className={`flex items-center justify-center flex-col ${image ? 'h-full' : 'h-[35vh] '}`}
+      className={`flex items-center justify-center flex-col ${image ? 'h-full' : 'h-[35vh] '}  w-full bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center`}
       px="small"
     >
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       {image && (
         <div className="max-w-[1100px] w-full mx-auto h-[60vh] mt-14 md:mt-10 mb-20">
           <ImageProvider
