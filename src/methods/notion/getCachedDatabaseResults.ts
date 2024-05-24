62d61d267ba127b47c2eb94897bf91fd9e9f1bb1
jwanner83 <jonas@wanner.com>
2024-05-24 21:56:06 +0930

    fix: remove default filter and sort

--- src/methods/notion/getCachedDatabaseResults.ts
+++ src/methods/notion/getCachedDatabaseResults.ts
@@ -3,7 +3,8 @@ import { Client } from '@notionhq/client'
 import { unstable_cache } from 'next/cache'
 
 const getCachedDatabaseResults = unstable_cache(
-  async (client: Client, id: string) => getDatabaseResults(client, id),
+  async (client: Client, id: string, options?: any) =>
+    getDatabaseResults(client, id, options),
   ['database-results'],
   {
     revalidate: 3600
