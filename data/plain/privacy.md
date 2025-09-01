729b986744e714d13872dc1c33516df8767b1746
jwanner83 <jonas@wanner.com>
2025-09-01 10:49:25 +0200

    adding imprint and privacy

---
+++ data/plain/privacy.md
@@ -0,0 +1,13 @@
+---
+title: Datenschutz
+description: Zuletzt aktualisiert am 1. September 2025
+---
+
+asdfasdf
+a
+sdf
+asdf
+asdf
+s
+
+## asdasdf
--- src/components/content/Container.tsx
+++ src/components/content/Container.tsx
@@ -3,6 +3,6 @@ import type { PropsWithChildren } from 'react'
 export default function Container({ children }: PropsWithChildren) {
   return <div className="prose dark:prose-invert
   prose-h1:font-bold prose-h1:text-xl
-  prose-a:underline prose-p:text-justify prose-p:text-dark prose-p:dark:text-light prose-img:rounded-large
-  prose-headings:font-bold prose-headings:text-dark prose-headings:dark:text-light">{children}</div>
+  prose-a:underline prose-p:text-justify prose-p:text-dark/80 prose-p:dark:text-light/80 prose-img:rounded-large
+  prose-headings:font-bold prose-headings:text-dark/80 prose-headings:dark:text-light/80">{children}</div>
 }
